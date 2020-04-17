b9e6a13e8e4bc24cb892fe53e781d9be3d4d823a
jakvah <jakobvahlin@gmail.com>
2020-04-17 23:13:15 +0000

    Recreated white gaussian noise

--- main.py
+++ main.py
@@ -25,7 +25,12 @@ CRLB_THETA = 12*(SIGMA_SQUARED)*((n_0**2)*N + 2*n_0*P + Q) / ((A**2)*(N**2)*((N*
 # ---------- Signals ---------- #
 
 # White complex Gaussian noise
-w = np.random.randn(N, 2).view(np.complex128)
+wReal = np.random.normal(0, 1, size=N)
+wImag = np.random.normal(0, 1, size=N)*1j
+
+w = []
+for i in range(N):
+    w.append([wReal[i] + wImag[i]])
 
 # Exponential signal
 s = []
@@ -39,6 +44,7 @@ for i in range(N):
 
 
 def main():
+    n = np.arange(N)
     print("The CRLB for the Omega estimator is: ", CRLB_OMEGA)
     print("The CRLB for the Theta estimator is: ",CRLB_THETA)
     
@@ -48,8 +54,8 @@ def main():
     plt.savefig("noise.png")
     
     plt.figure(3)
-    plt.plot(s)
-    plt.title("Onyl signal")
+    plt.stem(n,s)
+    plt.title("Plot of DT signal: $s[n] = e^{j(\omega n T -1)}$")
     plt.savefig("signal.png")
 
     plt.figure(2)
@@ -57,10 +63,30 @@ def main():
     plt.xlabel("n")
     plt.ylabel("x[n] = ")
     plt.plot(x)
-    plt.savefig("total.png")    
+    plt.savefig("total.png") 
+
+    n = np.arange(N)
+    plt.figure(4)
+    plt.title("Plot of DT signal: $x[n] = e^{j(\omega n T -1)} + w[n]$")
+    plt.xlabel("n")
+    plt.ylabel("x[n]")
+    plt.stem(n,x)
+    plt.savefig("stem.png")
+
+    # Fourier transform
 
 
+    FT_s = np.fft.fft(s)
+    FT_x = np.fft.fft(x)
 
+    plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.4, hspace=0.4)
+    plt.subplot(211)
+    plt.title("FFT of only signal")
+    plt.stem(n,FT_s)
+    plt.subplot(212)
+    plt.title("FFT of signal embedded in noise")
+    plt.stem(n,FT_x)
+    plt.savefig("fft.png")    
 
 main()
 
