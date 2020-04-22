Fixed variance estimation & CRLB

auto.py
@@ -9,7 +9,7 @@ import xlsxwriter
 dBs = [-10,0,10,20,30,40,50,60]
 lengthPowers = [10,12,14,16,18,20]
 
-ITERATIONS = 100
+ITERATIONS = 10
 
 # ---------- Constants ---------- #
 A = 1.0
@@ -28,9 +28,6 @@ P = (N*(N-1)) / 2
 Q = (N*(N-1)*(2*N-1)) / 6
 
 # ---------- CRLB ---------- #
-
-
-#CRLB_THETA = 12*(SIGMA_SQUARED)*((n_0**2)*N + 2*n_0*P + Q) / ((A**2)*(N**2)*((N**2)-1))
 
 def iterate(SIGMA_SQUARED,fft_length):
     # ---------- Signals ---------- #
@@ -54,10 +51,7 @@ def iterate(SIGMA_SQUARED,fft_length):
         x.append(s[i] + w[i])
 
     # Fourier transform
-    FT_s = np.fft.fft(s,n = fft_length)
     FT_x = np.fft.fft(x,fft_length)
-
-    FT_S = np.absolute(FT_s)
     FT_x = np.absolute(FT_x)
        
     # Finding most dominant in total signal
@@ -75,7 +69,7 @@ def main():
     ws.write(0, 2, "Mean estimated")
     ws.write(0, 3, "Mean error")
     ws.write(0, 4, "Variance")
-    ws.write(0, 5, "CRLB")
+    ws.write(0, 5, "CRLB [Hz^2]")
 
 
     lengthIterationIndex = 0
@@ -90,15 +84,17 @@ def main():
             CRLB_OMEGA = (12*(SIGMA_SQUARED)) / ((A**2)*(T**2)*N*((N**2)-1))
             CRLB_THETA = 12*(SIGMA_SQUARED)*((n_0**2)*N + 2*n_0*P + Q) / ((A**2)*(N**2)*((N**2)-1))
 
+            """
             print("*---------------------------------------*")
             print("Running ",ITERATIONS, "iterations with:")
             print("SNR [dB]:",SNR_db)
             print("FFT length:",fft_length,"(2^" + str(p) + ")")
             print("Frequency:",f_0/1000,"kHz")  
-            print("The CRLB for the Omega estimator is:", ((CRLB_OMEGA)/(2*np.pi))/1000,"kHz")
+            print("The CRLB for the Omega estimator is:", ((CRLB_OMEGA)/(4*np.pi**2)),"Hz^2")
             print("The CRLB for the Theta estimator is:",CRLB_THETA)
             print()
             print(" *--------------- RESULTS ---------------*")
+            """
 
 
             error=[]
@@ -107,23 +103,23 @@ def main():
                 f = iterate(SIGMA_SQUARED,fft_length)
 
                 err = f_0 - f
-                print("Iteration nr",(i+1),": ",f/1000,"kHz. Error:",(err/1000),"kHz")
+                #print("Iteration nr",(i+1),": ",f/1000,"kHz. Error:",(err/1000),"kHz")
                 
                 freq.append(f)
                 error.append(err) 
 
             freqmean = st.mean(freq)
-            print("Mean freq is: ",freqmean/1000,"kHz")
+            #print("Mean freq is: ",freqmean/1000,"kHz")
             errmean=st.mean(error)
-            print("Mean freq error is: ", errmean/1000, "kHz")
+            #print("Mean freq error is: ", errmean/1000, "kHz")
 
             errvar=st.variance(error, errmean)
-            print("The variance of the freq error is: ",errvar/1000, "kHz")
+            #print("The variance of the freq error is: ",errvar/1000, "kHz")
 
             ws.write(1 + dataIterationIndex +lengthIterationIndex*len(dBs), 2, freqmean)
             ws.write(1 + dataIterationIndex +lengthIterationIndex*len(dBs), 3, errmean)
             ws.write(1 + dataIterationIndex +lengthIterationIndex*len(dBs), 4, errvar)
-            ws.write(1 + dataIterationIndex +lengthIterationIndex*len(dBs), 5, CRLB_OMEGA)
+            ws.write(1 + dataIterationIndex +lengthIterationIndex*len(dBs), 5, (CRLB_OMEGA/(2*np.pi**2)))
 
             dataIterationIndex += 1
         lengthIterationIndex += 1

main.py
@@ -6,7 +6,7 @@ import statistics as st
 
 # ---------- Specifications ---------- #
 A = 1.0
-SNR_db = -10 # In dB
+SNR_db = 0 # In dB
 
 SNR_linear = 10.0**(SNR_db/10)
 SIGMA_SQUARED = (A**2)/(2*SNR_linear)
@@ -18,10 +18,10 @@ f_0 = 10**5
 omega_0 = 2*np.pi*f_0
 theta = np.pi/8
 
-k = 22
+k = 16
 fft_length = 2**k
 
-ITERATIONS = 10 
+ITERATIONS = 100 
 
 # ---------- CRLB Helpers ---------- #
 P = (N*(N-1)) / 2
@@ -29,7 +29,7 @@ Q = (N*(N-1)*(2*N-1)) / 6
 
 # ---------- CRLB ---------- #
 
-CRLB_OMEGA = (12*(SIGMA_SQUARED)) / ((A**2)*(T**2)*N*((N**2)-1))
+CRLB_OMEGA = (12*(SIGMA_SQUARED)) / ((A**2)*(T**2)*N*((N**2)-1)) # In Radians^2
 CRLB_THETA = 12*(SIGMA_SQUARED)*((n_0**2)*N + 2*n_0*P + Q) / ((A**2)*(N**2)*((N**2)-1))
 
 def iterate():
@@ -54,16 +54,9 @@ def iterate():
         x.append(s[i] + w[i])
 
     # Fourier transform
-    FT_s = np.fft.fft(s,n = fft_length)
     FT_x = np.fft.fft(x,fft_length)
-
-    FT_S = np.absolute(FT_s)
     FT_x = np.absolute(FT_x)
-       
-    # Checking that most dominant is as is to be expected
-    f_1 = bml.findDominantFrequency(FT_s,T,fft_length)
-    #("Most dominant frequency in s[in] is: ", f_1/1000, " kHz")
-
+    
     # Finding most dominant in total signal
     f_2 = bml.findDominantFrequency(FT_x,T,fft_length)
 
@@ -74,7 +67,7 @@ def main():
     print("SNR [dB]:",SNR_db)
     print("FFT length:",fft_length,"(2^" + str(k) + ")")
     print("Frequency:",f_0/1000,"kHz")  
-    print("The CRLB for the Omega estimator is:", ((CRLB_OMEGA)/(2*np.pi))/1000,"kHz")
+    print("The CRLB for the Omega estimator is:", CRLB_OMEGA/(4*np.pi**2),"Hz^2")
     print("The CRLB for the Theta estimator is:",CRLB_THETA)
     print()
     
@@ -87,13 +80,13 @@ def main():
         f = iterate()
         err = f_0 - f
         print("Iteration nr",(i+1),": ",f/1000,"kHz. Error:",(err/1000),"kHz")
-        freqs.append(f)
+        freqs.append(f) # In hertz
         error.append(err) 
 
     errmean=st.mean(error)
     print("Mean freq error is: ", errmean/1000, "kHz")
 
     errvar=st.variance(error, errmean)
-    print("The variance of the freq error is: ",errvar/1000, "kHz")
+    print("The variance of the freq error is: ",errvar, "Hz^2")
      
 main()