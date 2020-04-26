441bb61d79be8a101553d29a450f39f33afcd23f
jakvah <jakobvahlin@gmail.com>
2020-04-26 21:22:14 +0000

    Removed stuff from bml

--- buggesmatteland.py
+++ buggesmatteland.py
@@ -2,20 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-def GenerateNoise(signal, SNR_dB): # desiredSNR [dB]; signal is an array with complex values
-   n = np.zeros((len(signal),1), dtype=complex)
-   # SNR = var_signal / var_n
-   snr = 10.0**(SNR_dB/10.0) # Desired linear SNR
-   var_signal = signal.var() # Measure power of signal
-   var_n = var_signal / snr # Calculate required noise power for desired SNR
-   if (var_n == 0): # In case of a null signal
-       var_n = snr
-   e = np.random.normal(0, np.sqrt(var_n*2.0)/2.0, size=(len(signal), 2)).view(np.complex) # Generate noise with calculated power
-   for i in range(0, len(signal)):
-       n[i,0] = n[i,0] + e[i][0]
-   print(10.0*np.log10((np.var(signal))/(np.var(n))))
-   return n
-
 def meanSquareError(list1,list2):
     if len(list1) != len(list2):
         print("List lengths don't match. Exiting")
@@ -42,35 +28,3 @@ def sigmaSquaredFromdB(SNR_db,A):
     SIGMA_SQUARED = (A**2)/(2*SNR_linear)
     return SIGMA_SQUARED
 
-
-
-def circlePlot(angle):
-    N = 360
-    bottom = 0
-    max_height = 4
-
-    theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
-    radii = max_height*np.random.rand(N)
-    width = ((2*np.pi) / N) + 0.01
-
-    for i in range(N):
-        radii[i] = 0
-        if i == int(round(angle)):
-            radii[i] = (max_height*1)
-
-    ax = plt.subplot(111, polar=True)
-    bars = ax.bar(theta, radii, width=width, bottom=bottom)
-    ax.set_yticklabels([])
-    ax.set_title("Plot av vinkelen: " + str(int(round(angle))) + " i grader:")
-
-    # Spicy farger
-    for r, bar in zip(radii, bars): 
-        bar.set_facecolor(plt.cm.jet(r / 10.))
-        bar.set_alpha(0.8)
-    plt.savefig("angleplot.png")
-    plt.show()
-
-def makeAnglePositive(ang):
-    if ang < 0:
-        ang = 360 + ang
-    return ang
